1b4fe7c6a27ce6e4b086272d11caa6690580408d
BobisBilly <bobisbilly.3115@gmail.com>
2025-01-25 14:39:36 +1300

    modified:   main.js

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 exports.lotg = function(value) {
-    const characters = value.split("")
+    const characters = value.toString().split("")
     characters.splice((characters.length + 1) * Math.random() | 0, 0, 'T')
     console.log(characters.join(""))
 }
